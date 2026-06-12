381b4d8653d2ce743f4304e34f647f63b446a594
daffalde <daffalderayhan@gmail.com>
2026-06-12 16:46:01 +0700

    pembuatan result

--- app/components/Machine.tsx
+++ app/components/Machine.tsx
@@ -3,10 +3,51 @@
 import Image from "next/image";
 import style from "./machine.module.css";
 import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+type PredictResponse = {
+  code: number;
+  url: string;
+  result: string;
+  desc: string;
+  confidence: number;
+};
 
 export default function Machine() {
   const searchParams = useSearchParams();
   const product = searchParams.get("product");
+
+  const [url, setUrl] = useState<string>("");
+  const [dataApi, setDataApi] = useState<any>();
+
+  const handleClick = async () => {
+    try {
+      const res = await fetch(
+        "https://daffalde-linkphishing.hf.space/predict",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ url: url }),
+        },
+      );
+
+      const data: PredictResponse = await res.json();
+      console.log("API Result:", data);
+      setDataApi(data);
+    } catch (err: unknown) {
+      console.error("Error:", err);
+    }
+  };
+
+  const getColor = (code: number | undefined, alpha = 1) => {
+    if (code === 100 || code === 300) return `rgba(0, 184, 18, ${alpha})`;
+    if (code === 301) return `rgba(255, 35, 35, ${alpha})`;
+    if (code === 201 || code === 200) return `rgba(202, 192, 0, ${alpha})`;
+    return `rgba(144, 85, 253, ${alpha})`; // Default
+  };
+
   return (
     <>
       <div className={style.machine}>
@@ -108,6 +149,7 @@ export default function Machine() {
               name=""
               id=""
               placeholder="Enter your URL/Text...."
+              onChange={(e) => setUrl(e.target.value)}
             ></textarea>
             {product !== "url" ? null : (
               <button className={style.input_scan}>
@@ -120,7 +162,7 @@ export default function Machine() {
                 <p>QR Scan</p>
               </button>
             )}
-            <button className={style.input_analyze}>
+            <button onClick={handleClick} className={style.input_analyze}>
               <Image
                 src={"/analyze.webp"}
                 alt="ico analyze"
@@ -128,6 +170,38 @@ export default function Machine() {
                 height={20}
               />
             </button>
+          </div>
+          <div
+            className={`${style.result} ${dataApi ? style.result_show : ""}`}
+          >
+            <div
+              style={{ backgroundColor: getColor(dataApi?.code, 0.1) }}
+              className={style.result_title}
+            >
+              <p style={{ color: getColor(dataApi?.code, 1) }}>
+                {dataApi?.result}
+              </p>
+            </div>
+
+            <div className={style.result_bar}>
+              <div
+                style={{
+                  width: `${dataApi?.code === 200 || dataApi?.code === 201 ? "100" : dataApi?.confidence}%`,
+                  backgroundColor: getColor(dataApi?.code, 0.4),
+                  transition: "2s",
+                  transitionDelay: "0.5s",
+                }}
+                className={style.result_bar_in}
+              ></div>
+            </div>
+            <div className={style.result_conf}>
+              <p style={{ color: getColor(dataApi?.code, 1) }}>
+                {dataApi?.code === 200 || dataApi?.code === 201
+                  ? "-"
+                  : dataApi?.confidence}
+                %
+              </p>
+            </div>
           </div>
         </div>
       </div>

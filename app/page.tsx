ae2a47be40e51f866ab01ea3695df20a460ce31e
daffalde <daffalderayhan@gmail.com>
2026-06-12 11:08:25 +0700

    Add hover effects for documentation button and implement desktop navbar hover menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useParams, useSearchParams } from "next/navigation";
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const product = searchParams.get("product");
+
   return (
     <div className="container">
       <div className={styles.navbar}>
@@ -21,7 +28,12 @@ export default function Home() {
             </li>
           </ul>
         </div>
-        <button className={styles.documentation_button}>
+        <button
+          onClick={() => {
+            window.open("https://github.com/daffalde/komokuv2");
+          }}
+          className={styles.documentation_button}
+        >
           Documentation
           <img src="arrow-documentation.webp" alt="Documentation" />
         </button>
@@ -41,6 +53,62 @@ export default function Home() {
           </li>
         </ul>
         <p>© 2026 Komoku. Built for advanced phishing detection.</p>
+      </div>
+
+      {/* floating element */}
+      <div className={styles.desktop_navbar_hover}>
+        <ul>
+          <li>
+            <Link
+              className={
+                product === "url" || !product
+                  ? styles.desktop_navbar_hover_on
+                  : ""
+              }
+              href={"/?product=url"}
+            >
+              <Image
+                src={"/product-url.webp"}
+                alt="product url icon"
+                width={20}
+                height={20}
+              />
+              URL Scanner
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={
+                product === "email" ? styles.desktop_navbar_hover_on : ""
+              }
+              href={"/?product=email"}
+            >
+              <Image
+                src={"/product-mail.webp"}
+                alt="product mail icon"
+                width={20}
+                height={20}
+              />
+              Email Filter
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={
+                product === "sms" ? styles.desktop_navbar_hover_on : ""
+              }
+              href={"/?product=sms"}
+            >
+              <Image
+                src={"/product-sms.webp"}
+                alt="product sms icon"
+                width={20}
+                height={20}
+              />
+              SMS Guard
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
